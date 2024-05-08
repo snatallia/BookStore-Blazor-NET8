101b7bf0525e1574dd04d450556e7946a9320de3
Natallia <snatallia@gmail.com>
2024-05-08 22:05:32 +0200

    Added error handling in AuthorsController

--- BookStore.API/Controllers/AuthorsController.cs
+++ BookStore.API/Controllers/AuthorsController.cs
@@ -9,6 +9,7 @@ using BookStore.API.Data;
 using BookStore.API.Models.Author;
 using BookStore.API.Configurations;
 using AutoMapper;
+using BookStore.API.Static;
 
 namespace BookStore.API.Controllers
 {
@@ -18,35 +19,56 @@ namespace BookStore.API.Controllers
     {
         private readonly BookStoreDbContext _context;
         private readonly IMapper _mapper;
+        private readonly ILogger<AuthorsController> _logger;
 
-        public AuthorsController(BookStoreDbContext context, IMapper mapper)
+        public AuthorsController(BookStoreDbContext context, IMapper mapper, ILogger<AuthorsController> logger)
         {
             _context = context;
             _mapper = mapper;
+            _logger = logger;
         }
 
         // GET: api/Authors
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AuthorReadOnlyDto>>> GetAuthors()
         {
-            var authors = await _context.Authors.ToListAsync();
-            var authorDtos = _mapper.Map<IEnumerable<AuthorReadOnlyDto>>(authors);
-            return Ok(authorDtos);
+            try
+            {
+                var authors = await _context.Authors.ToListAsync();
+                var authorDtos = _mapper.Map<IEnumerable<AuthorReadOnlyDto>>(authors);
+                return Ok(authorDtos);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error performing GET in {nameof(GetAuthors)}");
+                return StatusCode(500, Messages.Error500Message);
+            }
+            
         }
 
         // GET: api/Authors/5
         [HttpGet("{id}")]
         public async Task<ActionResult<AuthorReadOnlyDto>> GetAuthorById(int id)
         {
-            var author = await _context.Authors.FindAsync(id);
-
-            if (author == null)
+            try
             {
-                return NotFound();
-            }
+                var author = await _context.Authors.FindAsync(id);
 
-            var authorDto = _mapper.Map<AuthorReadOnlyDto>(author);
-            return Ok(authorDto);
+                if (author == null)
+                {
+                    _logger.LogWarning($"Record Not Found: {nameof(GetAuthorById)} - ID: {id}");
+                    return NotFound();
+                }
+
+                var authorDto = _mapper.Map<AuthorReadOnlyDto>(author);
+                return Ok(authorDto);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error performing GET in {nameof(GetAuthorById)}");
+                return StatusCode(500, Messages.Error500Message);
+            }
+           
         }
 
         // PUT: api/Authors/5
@@ -56,6 +78,7 @@ namespace BookStore.API.Controllers
         {
             if (id != authorDto.Id)
             {
+                _logger.LogWarning($"Update ID invalid in {nameof(PutAuthor)} - ID: {id}");
                 return BadRequest();
             }
 
@@ -66,7 +89,7 @@ namespace BookStore.API.Controllers
             {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
+            catch (DbUpdateConcurrencyException ex)
             {
                 if (!await AuthorExists(id))
                 {
@@ -74,7 +97,8 @@ namespace BookStore.API.Controllers
                 }
                 else
                 {
-                    throw;
+                    _logger.LogError(ex, $"Error Performing GET in {nameof(PutAuthor)}");
+                    return StatusCode(500, Messages.Error500Message);
                 }
             }
 
@@ -86,27 +110,45 @@ namespace BookStore.API.Controllers
         [HttpPost]
         public async Task<ActionResult<Author>> PostAuthor(AuthorCreateDto authorDto)
         {
-            var author = _mapper.Map<Author>(authorDto);
-            _context.Authors.Add(author);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var author = _mapper.Map<Author>(authorDto);
+                _context.Authors.Add(author);
+                await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetAuthorById), new { id = author.Id }, author);
+                return CreatedAtAction(nameof(GetAuthorById), new { id = author.Id }, author);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error Performing POST in {nameof(PostAuthor)}", authorDto);
+                return StatusCode(500, Messages.Error500Message);
+            }
+            
         }
 
         // DELETE: api/Authors/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteAuthor(int id)
         {
-            var author = await _context.Authors.FindAsync(id);
-            if (author == null)
+            try
             {
-                return NotFound();
+                var author = await _context.Authors.FindAsync(id);
+                if (author == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Authors.Remove(author);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
             }
+            catch (Exception ex)
+            {
 
-            _context.Authors.Remove(author);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+                _logger.LogError(ex, $"Error Performing DELETE in {nameof(DeleteAuthor)}");
+                return StatusCode(500, Messages.Error500Message);
+            }
         }
 
         private async Task<bool> AuthorExists(int id)
---
+++ BookStore.API/Static/Messages.cs
@@ -0,0 +1,7 @@
+﻿namespace BookStore.API.Static
+{
+    public static class Messages
+    {
+        public const string Error500Message = "There was an error completing your request. Please, try again later.";
+    }
+}
