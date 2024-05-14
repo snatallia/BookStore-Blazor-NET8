08fa3df207c2071c4646a5281e3fe33ec62ec757
Natallia <snatallia@gmail.com>
2024-05-14 16:49:20 +0200

    Added Login and Logout components , aothorization and  authetication

--- BookStoreApp.Blazor.Server.UI/Program.cs
+++ BookStoreApp.Blazor.Server.UI/Program.cs
@@ -1,5 +1,9 @@
+using Blazored.LocalStorage;
 using BookStoreApp.Blazor.Server.UI.Components;
+using BookStoreApp.Blazor.Server.UI.Providers;
+using BookStoreApp.Blazor.Server.UI.Services.Authentication;
 using BookStoreApp.Blazor.Server.UI.Services.Base;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BookStoreApp.Blazor.Server.UI
 {
@@ -12,7 +16,16 @@ namespace BookStoreApp.Blazor.Server.UI
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
+
             builder.Services.AddHttpClient<IClient, Client>(cl => cl.BaseAddress = new Uri("https://localhost:7183/"));
+            builder.Services.AddBlazoredLocalStorage();
+
+            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddScoped<ApiAuthenticationStateProvider>();
+            builder.Services.AddScoped<AuthenticationStateProvider>(p =>
+                p.GetRequiredService<ApiAuthenticationStateProvider>());
+
+
 
             var app = builder.Build();
 
---
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,38 @@
+﻿using Blazored.LocalStorage;
+using BookStoreApp.Blazor.Server.UI.Providers;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IClient httpClient;
+        private readonly ILocalStorageService localStorage;
+        private readonly AuthenticationStateProvider authenticationStateProvider;
+
+        public AuthenticationService(IClient httpClient, ILocalStorageService localStorage, AuthenticationStateProvider authenticationStateProvider)
+        {
+            this.httpClient = httpClient;
+            this.localStorage = localStorage;
+            this.authenticationStateProvider = authenticationStateProvider;
+        }
+
+        public async Task<bool> AuthenticateAsync(LoginUserDto loginModel)
+        {
+            var response = await httpClient.LoginAsync(loginModel);
+            //store token in local storage
+            await localStorage.SetItemAsync("accessToken", response.Token);
+
+            //change auth state
+            await ((ApiAuthenticationStateProvider)authenticationStateProvider).LoggedIn();
+            return true;
+        }
+
+        public async Task<bool> LogoutAsync()
+        {
+            await ((ApiAuthenticationStateProvider)authenticationStateProvider).LoggedOut();
+            return true;
+        }
+    }
+}
